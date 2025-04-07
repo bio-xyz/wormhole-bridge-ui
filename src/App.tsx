e646cbae74bbace8d150e8b54c59165a80b0115f
sslexus <threesigma.tech@gmail.com>
2025-04-07 07:50:49 +0200

    use bio token

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,56 @@ import WormholeConnect, {
 } from "@wormhole-foundation/wormhole-connect";
 
 const wormholeConfig: WormholeConnectConfig = {
-  network: "Testnet",
-  chains: ["Solana", "BaseSepolia"],
-  tokens: ["WSV"],
+  network: "Mainnet",
+  chains: ["Ethereum", "Solana", "Base"],
+  rpcs: {
+    Ethereum: import.meta.env.VITE_PUBLIC_ETHEREUM_RPC_URL,
+    Solana: import.meta.env.VITE_PUBLIC_SOLANA_RPC_URL,
+    Base: import.meta.env.VITE_PUBLIC_BASE_RPC_URL,
+  },
   ui: {
-    title: "Wormhole NTT UI",
+    title: "",
     defaultInputs: {
-      fromChain: "Solana",
-      toChain: "BaseSepolia",
+      fromChain: "Ethereum",
+      toChain: "Solana",
     },
     walletConnectProjectId: import.meta.env
       .VITE_PUBLIC_WALLET_CONNECT_PROJECT_ID,
   },
-  // TODO: use a private RPC for mainnet
-  // rpcs: {
-  //   Solana: 'https://mainnet.helius-rpc.com/?api-key=$KEY',
-  // },
+  tokens: ["BIO"],
   routes: [
     ...nttRoutes({
       tokens: {
-        WSV_NTT: [
+        BIO_NTT: [
           {
-            chain: "Solana",
-            manager: "nMxHx1o8GUg2pv99y8JAQb5RyWNqDWixbxWCaBcurQx",
-            token: "2vLDzr7hUpLFHQotmR8EPcMTWczZUwCK31aefAzumkmv",
+            chain: "Ethereum",
+            manager: "0x1783E7d1F498321D7E15044d769621E1beDc7F4C",
+            token: "0xcb1592591996765Ec0eFc1f92599A19767ee5ffA",
             transceiver: [
               {
-                address: "AjL3f9FMHJ8VkNUHZqLYxa5aFy3aTN6LUWMv4qmdf5PN",
+                address: "0x676Cd89c6B6f02d6975547fD7Da1d5A8dbc8a3E1",
                 type: "wormhole",
               },
             ],
           },
           {
-            chain: "BaseSepolia",
-            manager: "0xaE02Ff9C3781C5BA295c522fB469B87Dc5EE9205",
-            token: "0xb8dccDA8C166172159F029eb003d5479687452bD",
+            chain: "Solana",
+            manager: "ntt11hdA4n1PupHhLyT1fsjg4YF9agVz3CTuzLRQs1H",
+            token: "bioJ9JTqW62MLz7UKHU69gtKhPpGi1BQhccj2kmSvUJ",
             transceiver: [
               {
-                address: "0xF4Af1Eac8995766b54210b179A837E3D59a9F146",
+                address: "5Yaf3N7MAEThp5FBBjUri8rv9mWxFEiJBjTKYYeKEi37",
+                type: "wormhole",
+              },
+            ],
+          },
+          {
+            chain: "Base",
+            manager: "0x9AfEbcA0d37661167AFD24481C39eBE2Ead89571",
+            token: "0x226A2FA2556C48245E57cd1cbA4C6c9e67077DD2",
+            transceiver: [
+              {
+                address: "0xcb75cC365abD9713b9A9B9613d90e9C359dd483F",
                 type: "wormhole",
               },
             ],
@@ -52,23 +64,32 @@ const wormholeConfig: WormholeConnectConfig = {
     }),
   ],
   tokensConfig: {
-    WSVsol: {
-      symbol: "WSV",
+    BIOeth: {
+      symbol: "BIO",
+      tokenId: {
+        chain: "Ethereum",
+        address: "0xcb1592591996765Ec0eFc1f92599A19767ee5ffA",
+      },
+      icon: "https://499247139-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F3ba2jNU6BPQUl4RXgHor%2Fuploads%2FnVRhDT0Cg1c1FtdQQOMd%2FToken%20Symbol%20BIO%20Round.svg?alt=media&token=58f7ce22-da87-4a8f-80eb-5a4df20659f6",
+      decimals: 18,
+    },
+    BIOsol: {
+      symbol: "BIO",
       tokenId: {
         chain: "Solana",
-        address: "2vLDzr7hUpLFHQotmR8EPcMTWczZUwCK31aefAzumkmv",
+        address: "bioJ9JTqW62MLz7UKHU69gtKhPpGi1BQhccj2kmSvUJ",
       },
-      icon: "https://wormhole.com/token.png",
+      icon: "https://499247139-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F3ba2jNU6BPQUl4RXgHor%2Fuploads%2FnVRhDT0Cg1c1FtdQQOMd%2FToken%20Symbol%20BIO%20Round.svg?alt=media&token=58f7ce22-da87-4a8f-80eb-5a4df20659f6",
       decimals: 9,
     },
-    WSVbase: {
-      symbol: "WSV",
+    BIObase: {
+      symbol: "BIO",
       tokenId: {
-        chain: "BaseSepolia",
-        address: "0xb8dccDA8C166172159F029eb003d5479687452bD",
+        chain: "Base",
+        address: "0x226A2FA2556C48245E57cd1cbA4C6c9e67077DD2",
       },
-      icon: "https://wormhole.com/token.png",
-      decimals: 9,
+      icon: "https://499247139-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F3ba2jNU6BPQUl4RXgHor%2Fuploads%2FnVRhDT0Cg1c1FtdQQOMd%2FToken%20Symbol%20BIO%20Round.svg?alt=media&token=58f7ce22-da87-4a8f-80eb-5a4df20659f6",
+      decimals: 18,
     },
   },
 };
